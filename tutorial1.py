2ace57af03d0e55d5b458ad34a04886a9d3ddf41
harsh <harsh@123gmail.com>
2022-04-30 08:48:16 -0600

    this is simple program

---
+++ tutorial1.py
@@ -0,0 +1,30 @@
+# to print a message on the screen
+
+print("this is my first program in python")
+print("i like make a programs")
+
+# to print our institute name
+
+print(" GOVENRNMENT POLYTECHNIC AURAI")
+
+# TO Print multiple statement
+
+print("hello ------!")
+print("What is  your name ---!")
+print("!-- Would you like to tell me ")
+print(" I am your best friend")
+
+# to display a value of single variable
+
+a=10
+print(a)
+b=20
+print(b)
+
+# to display a value of three variable
+
+num1=100
+num2=200
+num3=300
+print(num1,num2,num3)
+
